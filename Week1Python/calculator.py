0502a6a876902d0308cc16cd727951fdd282bd50
Willington Juma <willingtonejumamconyango@gmail.com>
2025-07-27 17:24:24 +0300

    Added week two assingnments

--- Week1Python/calculator.py
+++ Week1Python/calculator.py
@@ -1,11 +1,9 @@
-# Basic Calculator Program
 
-# Get user input
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter the operation (+, -, *, /): ")
 
-# Perform the operation
+
 if operation == '+':
     result = num1 + num2
     print(f"{num1} + {num2} = {result}")
